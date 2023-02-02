9a913d75befc92f6466811d787ee874d8ca4cac1
Eduardo Amaro Maciel <eduardo.maciel@edu.univali.br>
2023-02-02 12:58:44 -0300

    Update firebase.jsx

--- src/services/firebase.jsx
+++ src/services/firebase.jsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBkPP3LiUndHLq7yF0uIINXmq2pt6KZFmA",
-  authDomain: "teste-373623.firebaseapp.com",
-  projectId: "teste-373623",
-  storageBucket: "teste-373623.appspot.com",
-  messagingSenderId: "515199269205",
-  appId: "1:515199269205:web:796f6697582d0cf81c17dc",
-  measurementId: "G-VEYS2VGNM8"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
